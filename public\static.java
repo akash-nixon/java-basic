c1c4551e571a1d1e4ca0340ea5865801090e6fa2
nixonakash01 <106724526+nixonakash01@users.noreply.github.com>
2022-06-02 10:52:53 +0530

    Create public\static.java

---
+++ public\static.java
@@ -0,0 +1,14 @@
+class HelloWorld {
+   
+    public void fn(){
+        System.out.println("hi");
+    }
+    static void fn1(){
+        System.out.println("hoi");
+    }
+    public static void main(String[] args) {
+     //  System.out.println("Hello, World!"); 
+     HelloWorld obj=new HelloWorld();
+     obj.fn();
+    }
+}
